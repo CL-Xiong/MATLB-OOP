35a74e166eeee74ea92bf5175338d270fe412627
CL-Xiong <56728853+CL-Xiong@users.noreply.github.com>
2021-05-26 17:08:44 +0800

    Add files via upload

---
+++ 第三章/3.2.4/SimpleHandle.m
@@ -0,0 +1,14 @@
+classdef SimpleHandle < handle
+    properties
+        var
+    end
+    methods
+        function obj = SimpleValue(var)
+            obj.var = var;
+        end
+        function assignVar(obj, var)
+            obj.var = var;
+        end
+    end
+    
+end
